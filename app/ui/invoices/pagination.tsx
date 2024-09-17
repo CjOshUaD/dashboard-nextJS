6766686322d9aa6b83084e48e45140713ec99aab
CJDadia <joshdadia08@gmail.com>
2024-09-17 12:59:53 +1000

    new

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -3,7 +3,7 @@
 import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Link from 'next/link';
-import { generatePagination } from '@/app/lib/utils';
+import { generatePagination } from '@/app/lib/utils'
 import { usePathname, useSearchParams } from 'next/navigation';
 
 export default function Pagination({ totalPages }: { totalPages: number }) {
@@ -11,19 +11,19 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
   const searchParams = useSearchParams();
   const currentPage = Number(searchParams.get('page')) || 1;
 
-  const createPageURL = (pageNumber: number | string) =>{
+  const createPageURL = (pageNumber: number | string) => {
     const params = new URLSearchParams(searchParams);
     params.set('page', pageNumber.toString());
-    return '${pathname}?${params.toString()}';
+    return `${pathname}?${params.toString()}`;
   };
   // NOTE: Uncomment this code in Chapter 11
 
-   const allPages = generatePagination(currentPage, totalPages);
+    const allPages = generatePagination(currentPage, totalPages);
 
   return (
     <>
       {/*  NOTE: Uncomment this code in Chapter 11 */}
-
+      
        <div className="inline-flex">
         <PaginationArrow
           direction="left"
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,20 +1,16 @@
 'use client';
 
-import { useDebouncedCallback } from 'use-debounce';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
-  const searchParms = useSearchParams();
+  const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  function handleSearch(term: string){
-    const handleSearch = useDebouncedCallback((term)=>
-    {
-    console.log('Searching... ${term}');  
-    
-    const params = new URLSearchParams(searchParms);
+  const handleSearch = useDebouncedCallback((term) => {
+    const params = new URLSearchParams(searchParams);
     params.set('page', '1');
     if (term) {
       params.set('query', term);
@@ -23,7 +19,6 @@ export default function Search({ placeholder }: { placeholder: string }) {
     }
     replace(`${pathname}?${params.toString()}`);
   }, 300);
-}
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
@@ -31,12 +26,12 @@ export default function Search({ placeholder }: { placeholder: string }) {
         Search
       </label>
       <input
-        className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-        placeholder={placeholder}
-        onChange={(e)=>{
+        onChange={(e) => {
           handleSearch(e.target.value);
         }}
-        defaultValue={searchParms.get('query')?.toString()}
+        defaultValue={searchParams.get('query')?.toString()}
+        className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+        placeholder={placeholder}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
