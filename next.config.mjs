007b406c60d56855f889a8460d71797a4beac2b9
CJDadia <100544235+CjOshUaD@users.noreply.github.com>
2024-09-24 16:28:47 +1000

    put restriction

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+output: "export",  // <=== enables static exports
+  reactStrictMode: true,
     experimental:{
         ppr: 'incremental',
     },
 };
+
+module.exports = nextConfig;
 
 export default nextConfig;
