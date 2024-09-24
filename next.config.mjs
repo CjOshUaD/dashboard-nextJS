1f073ab8bcdecb18e0b03a4b6f103e782d8dcbbe
CJDadia <joshdadia08@gmail.com>
2024-09-24 17:46:08 +1000

    added features

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+    basePath: "/2048-in-react",
+    output: "export",  // <=== enables static exports
+  reactStrictMode: true,
+
     experimental:{
         ppr: 'incremental',
     },
 };
+
+module.exports = nextConfig;
 
 export default nextConfig;
